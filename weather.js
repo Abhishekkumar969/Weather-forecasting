3920c880ec3f1ebf0c107e21c11c4715cf236c7d
Abhishekkumar969 <dubeyji9631@gmail.com>
2023-05-24 03:45:36 +0530

    main

--- weather.js
+++ weather.js
@@ -44,8 +44,8 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Patna', opti
         wind_degrees02.innerHTML = response.wind_degrees
 
     })
-getWeather("Varanasi")
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Varanasi', options)
+getWeather("Gorakhpur")
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Gorakhpur', options)
     .then(response => response.json())
     .then((response) => {
         console.log(response)
